ac8381d96af3dc57d2b3e97761d687407b5063d5
SerafinoBarwinski <serafino@barwinski.com.de>
2026-05-20 17:42:36 +0000

    Only publish on tags, bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple-sip-client",
-    version="0.1.0",
+    version="0.1.1",
     description="Pure-Python SIP VoIP client library",
     author="simple-sip",
     packages=find_packages(where="src"),

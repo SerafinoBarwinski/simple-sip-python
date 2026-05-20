52689e9f1be2aee6dd94f358bf7aad64b427c7b1
SerafinoBarwinski <serafino@barwinski.com.de>
2026-05-20 17:28:39 +0000

    Rename package to simple-sip-client (simplesip already taken)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="simple-sip",
+    name="simple-sip-client",
     version="0.1.0",
     description="Pure-Python SIP VoIP client library",
     author="simple-sip",
